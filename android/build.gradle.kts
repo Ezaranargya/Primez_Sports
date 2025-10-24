5316f591e5a5ec4b20babe830e738d160ca41eb4
Ezar Anargya <email_github_kamu@example.com>
2025-10-24 13:34:37 +0700

    fix: product_model,admin_home_page,admin_product_add,edit_product_screen

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,9 +1,10 @@
+
 allprojects {
     repositories {
-    google()
-    mavenCentral()
-    maven { setUrl("https://jitpack.io") }
-}
+        google()
+        mavenCentral()
+        maven { setUrl("https://jitpack.io") }
+    }
 }
 
 val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
@@ -13,10 +14,29 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
+// Pastikan subprojects tergantung evaluation app
 subprojects {
     project.evaluationDependsOn(":app")
 }
 
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
+}
+
+project(":app") {
+    android {
+        buildTypes {
+            release {
+                signingConfig = signingConfigs.getByName("release")
+                minifyEnabled = true       // Aktifkan minify untuk optimize
+                shrinkResources = true     // Aktifkan shrink resources
+                // Gunakan file Proguard default + custom rules
+                proguardFiles(
+                    getDefaultProguardFile("proguard-android-optimize.txt"),
+                    "proguard-rules.pro"
+                )
+            }
+        }
+    }
 }
